d2cf0424cd3106c7097246867f1a543ce672739c
Jonathan Barel <jbarel@paypal.com>
2017-12-10 21:52:39 +0200

    fix validator typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 	before_save {self.email = email.downcase }
 	has_and_belongs_to_many :give_aways
 	validates :name, presence: true, length: {maximum: 80}
-	validates :email, presnce: true, length: {maximum: 250},
+	validates :email, presence: true, length: {maximum: 250},
 						uniqueness: {case_sensitive: false}
 
 	validates :password, length: {minimum: 8}
