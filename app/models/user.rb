8358057fb87b957a190d0899972ed0270e588b24
Jonathan Barel <jbarel@paypal.com>
2018-01-10 08:17:13 +0200

    remember me finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	attr_accessor :remember_token
+
 	before_save {self.email = email.downcase }
 	#has_and_belongs_to_many :exchanges
 
@@ -7,7 +9,10 @@ class User < ApplicationRecord
 	has_many :participating_exchanges, class_name: 'Exchange', through: :participations, source: :exchange
 	has_many :giftees, through: :participations
 
-
+	#VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  	#validates :email, presence: true, length: { maximum: 255 },
+    #                format: { with: VALID_EMAIL_REGEX },
+    #                uniqueness: { case_sensitive: false }
 
 	validates :name, presence: true, length: {maximum: 80}
 	validates :email, presence: true, length: {maximum: 250},
@@ -42,10 +47,28 @@ class User < ApplicationRecord
 		end
 	end
 
-	def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
 
+
+	def authenticated?(remember_token)
+		return false if remember_digest.nil?
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	def User.digest(string)
+    	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    	BCrypt::Password.create(string, cost: cost)
+  	end
+
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def forget
+		update_attribute(:remember_digest, nil)
+	end
 end
