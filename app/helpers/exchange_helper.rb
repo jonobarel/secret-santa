6e8f11e15dbbebb3e41909fa6188c2e71ceba6dd
Jonathan Barel <jbarel@paypal.com>
2018-01-20 00:17:55 +0200

    minimum participants

--- app/helpers/exchange_helper.rb
+++ app/helpers/exchange_helper.rb
@@ -1,2 +1,11 @@
 module ExchangeHelper
+	def get_assignment_button_text(ex)
+		if ex.locked?
+			"Assigned!"
+		elsif (cnt = ex.participations.count) <= 3
+			"Waiting for #{3 - cnt} more #{'participant'.pluralize}"
+		else
+			"Assign!"
+		end
+	end
 end
