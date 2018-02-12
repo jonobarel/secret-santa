94e36238edb69c6ec85969577eccddfaed003b10
Jonathan Barel <jbarel@paypal.com>
2018-02-12 17:54:18 +0200

    Revert "minimum participants"
    This reverts commit 6e8f11e15dbbebb3e41909fa6188c2e71ceba6dd.

--- app/helpers/exchange_helper.rb
+++ app/helpers/exchange_helper.rb
@@ -1,11 +1,2 @@
 module ExchangeHelper
-	def get_assignment_button_text(ex)
-		if ex.locked?
-			"Assigned!"
-		elsif (cnt = ex.participations.count) <= 3
-			"Waiting for #{3 - cnt} more #{'participant'.pluralize}"
-		else
-			"Assign!"
-		end
-	end
 end
