ef1c784a1c4b8e3924aff53565f984decefc628d
Jonathan Barel <jbarel@paypal.com>
2018-08-06 08:26:13 +0300

    clean up tutorial artifacts
    - removed reference to rails tutorial
    - redirect logged in users to their profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :redirect_to_profile_page, only: [:root]
   include SessionsHelper
 
+  def redirect_to_profile_page
+    if logged_in?
+      redirect_to_user
+    end
+  end
+  
   private
     def logged_in_user
     	unless logged_in?
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
+  before_action :redirect_to_profile_page, only: [:home]
+
+  def redirect_to_profile_page
+    if logged_in?
+      redirect_to current_user
+    end
+  end
+  
+
   def home
   end
 
